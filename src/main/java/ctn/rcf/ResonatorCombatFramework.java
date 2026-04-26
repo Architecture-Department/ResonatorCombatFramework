63d96a701770a0b45456c0ae9bf47962600de1d3
王小尽 <1809315974@qq.com>
2026-04-26 21:53:17 +0800

    修改和初步迁移

--- src/main/java/ctn/rcf/ResonatorCombatFramework.java
+++ src/main/java/ctn/rcf/ResonatorCombatFramework.java
@@ -1,16 +1,40 @@
 package ctn.rcf;
 
-import com.mojang.logging.LogUtils;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import org.slf4j.Logger;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-@Mod(ResonatorCombatFramework.MODID)
+@Mod(ResonatorCombatFramework.ID)
 public class ResonatorCombatFramework {
-    public static final String MODID = "resonator_combat_framework";
-    public static final Logger LOGGER = LogUtils.getLogger();
+  public static final String ID = "resonator_combat_framework";
+  public static final Logger LOGGER = LogManager.getLogger(ID);
 
     public ResonatorCombatFramework(IEventBus modEventBus, ModContainer modContainer) {
     }
+
+  @Contract("_ -> new")
+  public static @NotNull ResourceLocation modRl(final String name) {
+    return ResourceLocation.fromNamespaceAndPath(ID, name);
+  }
+
+  @Contract(pure = true)
+  public static @NotNull String modRlText(final String name) {
+    return ID + ":" + name;
+  }
+
+  public static <T> @NotNull DeferredRegister<T> modRegister(Registry<T> registry) {
+    return DeferredRegister.create(registry, ID);
+  }
+
+  public static <T> @NotNull DeferredRegister<T> modRegister(ResourceKey<Registry<T>> registry) {
+    return DeferredRegister.create(registry, ID);
+  }
 }
--- src/main/java/ctn/rcf/ResonatorCombatFrameworkClient.java
+++ src/main/java/ctn/rcf/ResonatorCombatFrameworkClient.java
@@ -5,8 +5,8 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 
-@Mod(value = ResonatorCombatFramework.MODID, dist = Dist.CLIENT)
-@EventBusSubscriber(modid = ResonatorCombatFramework.MODID, value = Dist.CLIENT)
+@Mod(value = ResonatorCombatFramework.ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = ResonatorCombatFramework.ID, value = Dist.CLIENT)
 public class ResonatorCombatFrameworkClient {
     public ResonatorCombatFrameworkClient(ModContainer container) {
     }
