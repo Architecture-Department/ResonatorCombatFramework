e50773769cec1f827ddd1fa15ea1a5467a5cf480
王小尽 <1809315974@qq.com>
2026-04-29 13:58:12 +0800

    （（（

--- src/main/java/architecture/resonator_combat_framework/core/ResonatorCombatFramework.java
+++ src/main/java/architecture/resonator_combat_framework/core/ResonatorCombatFramework.java
@@ -24,11 +24,6 @@ public final class ResonatorCombatFramework {
 		NeoForge.EVENT_BUS.register(this);
 	}
 
-	@SubscribeEvent
-	public void onServerStarting(ServerStartingEvent event) {
-		LOGGER.info("HELLO from server starting");
-	}
-
 	@Contract("_ -> new")
 	public static @NotNull ResourceLocation modRl(final String name) {
 		return ResourceLocation.fromNamespaceAndPath(ID, name);
@@ -45,5 +40,10 @@ public final class ResonatorCombatFramework {
 
 	public static <T> @NotNull DeferredRegister<T> modRegister(ResourceKey<Registry<T>> registry) {
 		return DeferredRegister.create(registry, ID);
+	}
+
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event) {
+		LOGGER.info("HELLO from server starting");
 	}
 }
